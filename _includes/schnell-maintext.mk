1dbe076402b215ff9a44e4ef102d45a173dd2db2
pascal-codecat <pascal.zwicker@windowslive.com>
2017-08-12 16:14:15 +0200

    added text

--- _includes/schnell-maintext.mk
+++
@@ -1,4 +0,0 @@
-Um einen Anwender von sich oder seinen Produkten/Leistungen zu überzeugen, bleiben
-einer Webseite nur wenige Sekunden. Je nach Literatur/Studie und je nachdem, wie
-eng man den zweiten Eindruck fasst, kann man hier von ca. 2 bis gar 10 Sekunden
-ausgehen.
